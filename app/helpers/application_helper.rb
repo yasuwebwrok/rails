0dcd8c2c7f3735bdb2d4e7be20cc9aa43ed0911f
yasuwebwrok <yasu.bobujk@gmail.com>
2018-12-24 22:21:02 +0900

    a

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+
+  #ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "SingStreet"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+end
